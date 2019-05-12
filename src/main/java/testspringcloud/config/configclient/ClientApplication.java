631936372a73bad4e6757e1245a88d78abbd1a42
cauchy8389 <cauchy_8389>
2019-05-12 15:25:23 +0800

    如果需要 config bus 那么需要放开maven中： <dependency>     <groupId>org.springframework.cloud</groupId>     <artifactId>spring-cloud-starter-bus-kafka</artifactId>     <version>2.1.1.RELEASE</version> </dependency> --------------------- 作者：海の云 来源：CSDN 原文：https://blog.csdn.net/cauchy8389/article/details/90140089 版权声明：本文为博主原创文章，转载请附上博文链接！

--- src/main/java/testspringcloud/config/configclient/ClientApplication.java
+++ src/main/java/testspringcloud/config/configclient/ClientApplication.java
@@ -1,10 +1,14 @@
 package testspringcloud.config.configclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@RefreshScope
 public class ClientApplication {
 
 	@Autowired
 	private Environment env;
 
+	@Value("${mybook}")
+	private String mybook;
+
 	@RequestMapping("/")
 	public String home() {
 		String name = env.getProperty("test.user.name");
-		return "Hello " + name;
+		return "Hello " + name + mybook;
 	}
 	
 	public static void main(String[] args) {
@@ -29,4 +37,11 @@ public class ClientApplication {
 		//VM option :
 		//-Dspring.cloud.bootstrap.location=classpath:/springcloud/config-client-boot.yml
 	}
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setIgnoreUnresolvablePlaceholders(true);
+        return c;
+    }
 }
--- src/main/java/testspringcloud/config/configserver/ConfigApplication.java
+++ src/main/java/testspringcloud/config/configserver/ConfigApplication.java
@@ -2,12 +2,12 @@ package testspringcloud.config.configserver;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
+//import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableConfigServer
+//@EnableConfigServer      //另外开一个项目 maven 引入 config server
 public class ConfigApplication {
 
 	public static void main(String[] args) {
