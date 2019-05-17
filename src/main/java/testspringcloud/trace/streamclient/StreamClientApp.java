5d9787eeed01f0dba05996cf81d2407008639fe7
cauchy8389 <cauchy8389@sina.com>
2019-05-17 18:02:57 +0800

    spring cloud sleuth zipkin2 commit

---
+++ src/main/java/testspringcloud/trace/streamclient/StreamClientApp.java
@@ -0,0 +1,23 @@
+package testspringcloud.trace.streamclient;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StreamClientApp {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/hello")
+	public String hell() {
+		System.out.println("调用业务方法");
+		return "Hello";
+	}
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(StreamClientApp.class)
+				.properties("spring.config.location=classpath:/springcloud/trace-stream-client.yml").run(args);
+	}
+}
